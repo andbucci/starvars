578869bbb5f391800797a1d143a12595ac2981ca
Andrea Bucci <a.bucci@univpm.it>
2019-07-18 16:39:24 +0200

    multiCUMSUM function

--- R/multiCUMSUM.R
+++ R/multiCUMSUM.R
@@ -13,6 +13,25 @@ quiet <- function(x) {
   on.exit(sink())
   invisible(force(x))
 }
+lrvarbart <- function(x){
+  N <- length(x)
+  cc = 1.4
+  K = N^(1/3)
+  ac <- acf(x, plot = F, lag.max = floor(2 * N^(2/3)))$acf
+  vc <- cc * sqrt(log(N, base = 10)/N)
+  for (i in 1:floor(N^(2/3))) {
+    if (max(abs(ac[i + (1:K)])) < vc) {
+      break
+    }
+  }
+  w <- ((2 * i):1)/(2 * i)
+  ac <- acf(x, plot = F, type = "covariance", lag.max = 2 *
+              i)$acf[1:(2 * i + 1)]
+  asy <- ac[1] + 2 * sum(ac[2:(2 * i + 1)] * w)
+  erg <- list(lrv = asy, bandwidth = i)
+  return(erg)
+}
+}
 for(i in 1:ncoly){
   gspec <- quiet(garchFit(~garch(1,1), data = data[,i]))
   h1[,i] <- gspec@h.t
