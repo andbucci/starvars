ad96f92a5c351357fe9412d46663fef692a9612d
andbucci <a.bucci@univpm.it>
2020-05-25 16:41:47 +0200

    Added plot, coef, logLik functions

--- R/plot.VLSTAR.R
+++ R/plot.VLSTAR.R
@@ -1,9 +1,9 @@
 #'@S3method predict VLSTAR
 
 plot.VLSTAR <- function(object, names = NULL, main.fit = NULL, main.acf = NULL, main.pacf = NULL, main.logi = NULL, ylim.fit = NULL, ylim.resid = NULL, lty.fit = NULL, lty.resid = NULL, lty.logi = NULL, lwd.fit = NULL, lwd.resid = NULL, lwd.logi = NULL, lag.acf = NULL, lag.pacf = NULL, col.fit = NULL, col.resid = NULL, col.logi = NULL,  ylab.fit = NULL, ylab.resid = NULL, ylab.acf = NULL, ylab.pacf = NULL, ylab.logi = NULL, xlab.fit = NULL, xlab.resid = NULL, xlab.logi = NULL, mar = par("mar"), oma = par("oma"), adj.mtext = NA, padj.mtext = NA, col.mtext = NA,...){
-  op <- par(no.readonly = TRUE)
-  resids <- resid(object)
-  fitted <- fitted(object)
+  op <- graphics::par(no.readonly = TRUE)
+  resids <- stats::resid(object)
+  fitted <- stats::fitted(object)
   y <- object$Data[[1]]
   ynames <- colnames(y)
   colnames(fitted) <- ynames
@@ -56,21 +56,22 @@ plot.VLSTAR <- function(object, names = NULL, main.fit = NULL, main.acf = NULL, 
                       ylab.acf, ylab.pacf, xlab.fit, xlab.resid, xlab.logi, adj.mtext, padj.mtext, col.mtext, ...){
     ifelse(is.null(ylim.fit), ylim.fit <- c(min(c(y, fitted)), max(c(y, fitted))), ylim.fit <- ylim.fit)
     ifelse(is.null(ylim.resid), ylim.resid <- c(min(resids), max(resids)), ylim.resid <- ylim.resid)
-    layout(matrix(c(1, 1, 2, 2, 3, 4, 5, 5), nrow = 4, ncol = 2, byrow = TRUE))
-    par(oma = c(6, 0, 5, 0), mar = c(0, 5.1, 0, 2.1))
-    plot.ts(y, main = "", ylim = ylim.fit, ylab = ylab.fit, xlab = xlab.fit, lty = lty.fit[1], lwd = lwd.fit[1], col = col.fit[1], axes = FALSE)
-    lines(fitted, col = col.fit[2], lty = lty.fit[2], lwd = lwd.fit[2])
-    box()
-    axis(2, pretty(c(y, fitted))[-1])
-    mtext(main.fit, side = 3, line = 3, adj = adj.mtext, padj = padj.mtext, col = col.mtext, ...)
-    plot.ts(resids, main = "", ylim = ylim.resid, ylab = ylab.resid, xlab = xlab.resid, lty = lty.resid[1], lwd = lwd.resid[1], col = col.resid[1])
-    abline(h = 0, col = col.resid[2], lty = lty.resid[2], lwd = lwd.resid[2])
-    par(mar=c(1, 5.1, 5, 2.1))
-    acf(resids, main = main.acf, ylab = ylab.acf, lag.max = lag.acf, ...)
-    pacf(resids, main = main.pacf, ylab = ylab.pacf, lag.max = lag.pacf, ...)
-    plot(1:length(logistic),logistic, main = main.logi, ylim = c(0,1), ylab = ylab.logi, xlab = xlab.logi, lty = lty.logi[1], lwd = lwd.logi[1], col = col.logi[1], axes = T)
+    graphics::layout(matrix(c(1, 1, 2, 2, 3, 4, 5, 5), nrow = 4, ncol = 2, byrow = TRUE))
+    graphics::par(oma = c(5, 0, 4, 0), mar = c(0, 5.1, 0, 2.1))
+    stats::plot.ts(y, main = "", ylim = ylim.fit, ylab = ylab.fit, xlab = xlab.fit, lty = lty.fit[1], lwd = lwd.fit[1], col = col.fit[1], axes = FALSE)
+    graphics::lines(fitted, col = col.fit[2], lty = lty.fit[2], lwd = lwd.fit[2])
+    graphics::box()
+    graphics::axis(2, pretty(c(y, fitted))[-1])
+    graphics::mtext(main.fit, side = 3, line = 3, adj = adj.mtext, padj = padj.mtext, col = col.mtext, ...)
+    stats::plot.ts(resids, main = "", ylim = ylim.resid, ylab = ylab.resid, xlab = xlab.resid, lty = lty.resid[1], lwd = lwd.resid[1], col = col.resid[1])
+    graphics::abline(h = 0, col = col.resid[2], lty = lty.resid[2], lwd = lwd.resid[2])
+    graphics::par(mar=c(1, 5.1, 5, 2.1))
+    stats::acf(resids, main = main.acf, ylab = ylab.acf, lag.max = lag.acf, ...)
+    stats::pacf(resids, main = main.pacf, ylab = ylab.pacf, lag.max = lag.pacf, ...)
+    graphics::par(oma = c(6, 0, 5, 0), mar = c(0, 5.1, 0, 2.1))
+    graphics::plot(1:length(logistic),logistic, main = main.logi, ylim = c(0,1), ylab = ylab.logi, xlab = xlab.logi, lty = lty.logi[1], lwd = lwd.logi[1], col = col.logi[1], axes = T)
     }
-  par(mar = mar, oma = oma)
+  graphics::par(mar = mar, oma = oma)
   for (i in 1:nv) {
     plotest(y = y[, names[i]], fitted = fitted[, names[i]], resids = resids[, names[i]], logistic = logistic[, names[i]], main.fit = main.fit[i], main.acf = main.acf[i], main.pacf = main.pacf[i], main.logi = main.logi[i], ylab.fit = ylab.fit[i], ylab.resid = ylab.resid[i], ylab.acf = ylab.acf[i], ylab.pacf = ylab.pacf[i], ylab.logi = ylab.logi[i], xlab.fit = xlab.fit[i], xlab.resid = xlab.resid[i], xlab.logi = xlab.logi[i], adj.mtext = adj.mtext, padj.mtext = padj.mtext, col.mtext = col.mtext)
     if (nv > 1) par(ask = TRUE)
