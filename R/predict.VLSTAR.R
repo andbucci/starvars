60dd0e06313495aabca9ff9362fcd1271834b441
andbucci <a.bucci@univpm.it>
2020-11-24 18:21:39 +0100

    Fixed regimes bugs

--- R/predict.VLSTAR.R
+++ R/predict.VLSTAR.R
@@ -34,11 +34,10 @@ newdata <- as.matrix(newdata)
   BB <- object$B
   BB1 <- object$B[,seq(1, ncol(BB))[c(rep(TRUE, ncoly), rep(FALSE, ncoly))]]
   BB2 <- object$B[,seq(1, ncol(BB))[c(rep(FALSE, ncoly), rep(TRUE, ncoly))]]
-  In <- diag(ncol(object$Data[[1]]))
-  Gtilde <- t(cbind(In, object$Gtilde[[]]))
+  Gtilde <- t(object$Gtilde[[length(object$Gtilde)]])
   pred <- matrix(NA, ncol = ncoly, nrow = n.ahead)
   Z <- c(constant[1, ], yy, newdata)
-  pred[1,] <- as.matrix((t(Gtilde)%*%t(BB)%*%Z))
+  pred[1,] <- as.matrix((Gtilde%*%t(BB)%*%Z))
   tmp <- pred[1, ]
   yy <- c(tmp)
   if(is.null(st.new)){
