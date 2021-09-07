144b1a9644582f77d5da1236a26f0d8210c3a0c8
andbucci <a.bucci@univpm.it>
2021-09-07 16:35:03 +0200

    Reduced runtime and fixed bugs

--- R/misc.R
+++ R/misc.R
@@ -40,22 +40,29 @@ SSQ <- function(param, data){
   c <- param[(ncol(y)*(data$m-1)+1):length(param)]
   gamma1 <- matrix(gamma, ncol = (data$m-1))
   c1 <- matrix(c, ncol = (data$m-1))
-  glog <- rep(0, ncol(y))
   GT <- list()
-  dify <- matrix(ncol = 1, nrow = nrow(y))
-  for (z in 1:nrow(y)){
+  sumdif <- sum(sapply(1:nrow(y), function(z){
     for(t in 1:(data$m-1)){
-      for (o in 1:ncol(y)){
-        glog[o] <- (1L+exp(-gamma1[o,t]*(data$st[z]-c1[o,t])))^(-1)}
-      if(data$singlecgamma == TRUE){
-        GT[[t]] <- diag(rep(glog[1], ncol(y)))
-      }else{
-        GT[[t]] <- diag(glog)
-      }
-    }
-    Gtilde <- t(cbind(diag(ncol(y)), do.call(cbind,GT)))
-    dify[z] <-  t(y[z, ] - t(Gtilde)%*%t(data$BB)%*%data$x[z,])%*%(y[z, ] - t(Gtilde)%*%t(data$BB)%*%data$x[z,])
-  }
-  sumdif <- sum(dify)
+      ifelse(data$singlecgamma == TRUE, GT[[t]] <-diag(rep(sapply(1:6, function(o){(1L+exp(-gamma1[o,t]*(data$st[z]-c1[o,t])))^(-1)}), ncol(y))),
+             GT[[t]] <- diag(sapply(1:6, function(o){(1L+exp(-gamma1[o,t]*(data$st[z]-c1[o,t])))^(-1)})))}
+    t(y[z, ] - t(t(cbind(diag(ncol(y)), do.call(cbind,GT))))%*%t(data$BB)%*%data$x[z,])%*%(y[z, ] - t(t(cbind(diag(ncol(y)), do.call(cbind,GT))))%*%t(data$BB)%*%data$x[z,])
+  }))
+# glog <- rep(0, ncol(y))
+#   GT <- list()
+#   dify <- matrix(ncol = 1, nrow = nrow(y))
+#   for (z in 1:nrow(y)){
+#     for(t in 1:(data$m-1)){
+#       for (o in 1:ncol(y)){
+#         glog[o] <- (1L+exp(-gamma1[o,t]*(data$st[z]-c1[o,t])))^(-1)}
+#       if(data$singlecgamma == TRUE){
+#         GT[[t]] <- diag(rep(glog[1], ncol(y)))
+#       }else{
+#         GT[[t]] <- diag(glog)
+#       }
+#     }
+#     Gtilde <- t(cbind(diag(ncol(y)), do.call(cbind,GT)))
+#     dify[z] <-  t(y[z, ] - t(Gtilde)%*%t(data$BB)%*%data$x[z,])%*%(y[z, ] - t(Gtilde)%*%t(data$BB)%*%data$x[z,])
+#   }
+#   sumdif <- sum(dify)
   return(sumdif)
 }
